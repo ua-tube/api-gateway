ccbd8c3a6c4ccfdd675e2da41b3d67414e1adb4d
Oleh Voloshyn <bazisc.qa@gmail.com>
2024-05-18 13:31:47 +0300

    Remove: comment

--- src/constants.ts
+++ src/constants.ts
@@ -9,15 +9,3 @@ export const serviceNames: string[] = [
   'history',
   'search',
 ];
-
-// export const serviceAndEnvNameKeyPair: ServiceAndEnvNameKeyPair = [
-//   ['community', 'COMMUNITY_SVC_ORIGIN'],
-//   ['library', 'LIBRARY_SVC_ORIGIN'],
-//   ['subscriptions', 'SUBSCRIPTIONS_SVC_ORIGIN'],
-//   ['users', 'USERS_SVC_ORIGIN'],
-//   ['video-manager', 'VIDEO_MANAGER_SVC_ORIGIN'],
-//   ['video-processor', 'VIDEO_PROCESSOR_SVC_ORIGIN'],
-//   ['video-store', 'VIDEO_STORE_SVC_ORIGIN'],
-//   ['history', 'HISTORY_SVC_ORIGIN'],
-//   ['search', 'SEARCH_SVC_ORIGIN'],
-// ];
